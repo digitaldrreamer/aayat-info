9f9627f3a1d6fd02d7300edc6518a7a97e7eaa6a
digitaldrreamer <digitaldrreamer@gmail.com>
2025-02-16 16:20:07 +0100

    ✨ feat(layout): Remove Pitch button and add Developer Notice button in navigation

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -648,16 +648,18 @@
 			</a>
 
 			<div class="flex items-center gap-2">
-				<Button title="Pitch" onclick={() => (goto('/pitch'))} variant="ghost" size="icon">
-					<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-							 stroke="currentColor" class="size-6">
-						<path stroke-linecap="round" stroke-linejoin="round"
-									d="M3.75 3v11.25A2.25 2.25 0 0 0 6 16.5h2.25M3.75 3h-1.5m1.5 0h16.5m0 0h1.5m-1.5 0v11.25A2.25 2.25 0 0 1 18 16.5h-2.25m-7.5 0h7.5m-7.5 0-1 3m8.5-3 1 3m0 0 .5 1.5m-.5-1.5h-9.5m0 0-.5 1.5M9 11.25v1.5M12 9v3.75m3-6v6" />
-					</svg>
-				</Button>
+<!--				<Button title="Pitch" onclick={() => (goto('/pitch'))} variant="ghost" size="icon">-->
+<!--					<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"-->
+<!--							 stroke="currentColor" class="size-6">-->
+<!--						<path stroke-linecap="round" stroke-linejoin="round"-->
+<!--									d="M3.75 3v11.25A2.25 2.25 0 0 0 6 16.5h2.25M3.75 3h-1.5m1.5 0h16.5m0 0h1.5m-1.5 0v11.25A2.25 2.25 0 0 1 18 16.5h-2.25m-7.5 0h7.5m-7.5 0-1 3m8.5-3 1 3m0 0 .5 1.5m-.5-1.5h-9.5m0 0-.5 1.5M9 11.25v1.5M12 9v3.75m3-6v6" />-->
+<!--					</svg>-->
+<!--				</Button>-->
+				{#if $isInstalled && !$installPrompt}
 				<Button title="Developer Notice" variant="ghost" onclick={() => (goto('/dev-notice'))} size="icon">
 					<CircleHelp class="size-5" />
 				</Button>
+					{/if}
 				<Button title="Sitemap" href="/sitemap.xml" variant="link">
 					<Rss class="size-5" />
 				</Button>
