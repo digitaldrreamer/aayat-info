40ca9b5d5c17aae566be4a5edc109cfa72368e37
digitaldrreamer <digitaldrreamer@gmail.com>
2025-02-25 09:10:14 +0100

    ✨ feat(server): Implement caching for sitemap generation and update XML structure

--- src/routes/sitemap.xml/+server.js
+++ src/routes/sitemap.xml/+server.js
@@ -1,22 +1,32 @@
 import { url } from '$lib/url.js';
 
+let cachedSitemap = null;
+
 export const GET = async ({ fetch }) => {
-	const req = await fetch(url + '/api/v1/search')
-	const res = await req.json()
-	const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+	if (!cachedSitemap) {
+		const req = await fetch(url + '/api/v1/search');
+		const res = await req.json();
+
+		cachedSitemap = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-<url>
-<loc>https://aayah.info</loc>
-<changefreq>daily</changefreq>
-<priority>0.7</priority>
-</url>
-${res.map(link  =>  `
-<url>
-<loc>${link}</loc>
-<changefreq>weekly</changefreq>
-<lastmod>2025/05/16</lastmod>
-</url>
-`).join('')}
+  <url>
+    <loc>https://aayah.info</loc>
+    <changefreq>never</changefreq>
+    <priority>0.7</priority>
+  </url>
+  ${res.map(object => `
+  <url>
+    <loc>${object.url}</loc>
+    <changefreq>never</changefreq>
+    <lastmod>2025-05-16</lastmod>
+  </url>`).join('')}
 </urlset>`;
-	return new Response(sitemap);
-}
+	}
+
+	return new Response(cachedSitemap, {
+		headers: {
+			'Content-Type': 'application/xml',
+			'Cache-Control': 'public, max-age=86400' // Cache for 24 hours
+		}
+	});
+};
